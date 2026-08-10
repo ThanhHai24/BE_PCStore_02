e5ed58852967059203ed12998fedb97dc36c86c9
vuhuq25133 <vuhuq2104@gmail.com>
2026-08-10 10:49:21 +0700

    feature: vnpay sandbox

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.routes";
 import productRoutes from "./routes/product.routes";
 import categoryRoutes from "./routes/category.routes";
 import orderRoutes from "./routes/order.routes";
+import paymentRoutes from "./routes/payment.routes";
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/payment", paymentRoutes);
 
 app.get("/", (req, res) => {
     res.json({
